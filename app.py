5ef89ed198108ad90ed2c4c55220d57caf902343
mostafa1234307200 <Almasrikhalel332@gmail.com>
2025-05-09 18:28:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+import io
 
+# إعداد الصفحة
+st.set_page_config(page_title="مدرسة الأردن الأساسية المختلطة", layout="centered")
+
+# تنسيق
+st.markdown("""
+    <style>
+        .stApp {
+            background-color: #f0f8ff;
+        }
+        .title {
+            text-align: center;
+            color: #0d47a1;
+        }
+        .upload-box {
+            background-color: #ffffffcc;
+            padding: 20px;
+            border-radius: 10px;
+            border: 2px dashed #90caf9;
+        }
+    </style>
