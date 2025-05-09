52c32594f5aac690223ca2fbd7ec94122eddafd2
mostafa1234307200 <Almasrikhalel332@gmail.com>
2025-05-09 17:31:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,17 +21,3 @@ st.markdown("""
             background-color: #f1f8e9;
             margin-top: 20px;
             text-align: center;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-st.markdown("<h1 class='title'>مدرسة الأردن الأساسية المختلطة</h1>", unsafe_allow_html=True)
-st.markdown("<h3 class='title'>كيف سيبدو وجهك بعد 30 سنة من التدخين؟</h3>", unsafe_allow_html=True)
-
-st.markdown("<div class='upload-box'>", unsafe_allow_html=True)
-uploaded_file = st.file_uploader("ارفع صورة (jpg أو png)", type=["jpg", "jpeg", "png"])
-camera_input = st.camera_input("أو التقط صورة بالكاميرا")
-st.markdown("</div>", unsafe_allow_html=True)
-
-def apply_enhanced_smoking_effect(img):
-    img = cv2.cvtColor(img,
